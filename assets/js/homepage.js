71937fcefc45252b27a1165da58c2df1b4ff3eeb
walterego <walter.scimone@gmail.com>
2021-03-16 19:29:58 -0400

    improved fetch username method

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,14 @@
-var getUserRepos = function() {
+var getUserRepos = function(user) {
     // format the github api url
-    var apiUrl = "https://api.github.com/users/microsoft" + "/repos";
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
     // make a request to the url
     fetch(apiUrl).then(function(response) {
+        console.log(response)
       response.json().then(function(data) {
         console.log(data);
       });
     });
   };
 
-  getUserRepos();
+  getUserRepos("microsoft");
